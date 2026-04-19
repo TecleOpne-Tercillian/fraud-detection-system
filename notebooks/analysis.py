cc7cdcccf8b368fa088c5ce46c29ca387d563e03
Magna Tercillia <estudotopne@outlook.com>
2026-04-19 15:31:19 -0300

    .

--- notebooks/analysis.py
+++ notebooks/analysis.py
@@ -4,31 +4,31 @@ import matplotlib.pyplot as plt
 # carregar dataset
 df = pd.read_csv("data/transactions.csv")
 
-print("\n📌 Primeiras linhas:")
+print("\nPrimeiras linhas:")
 print(df.head())
 
-print("\n📌 Informações gerais:")
+print("\nPrimeiras linhas:")
 print(df.info())
 
-print("\n📌 Distribuição de fraude:")
+print("\nDistribuição de fraude:")
 print(df["is_fraud"].value_counts())
 
 # ----------------------------
-# 📊 1. Valor médio por fraude
+# 1. Valor médio por fraude
 # ----------------------------
-print("\n📊 Média de valores:")
+print("\nMédia de valores:")
 print(df.groupby("is_fraud")["amount"].mean())
 
 # ----------------------------
-# 📊 2. Quantidade de fraudes por usuário
+# 2. Quantidade de fraudes por usuário
 # ----------------------------
 fraud_by_user = df.groupby("user_id")["is_fraud"].sum().sort_values(ascending=False)
 
-print("\n📊 Top usuários com fraude:")
+print("\nTop usuários com fraude:")
 print(fraud_by_user.head())
 
 # ----------------------------
-# 📊 3. Gráfico de fraude vs não fraude
+# 3. Gráfico de fraude vs não fraude
 # ----------------------------
 df["is_fraud"].value_counts().plot(
     kind="bar",
@@ -38,14 +38,14 @@ df["is_fraud"].value_counts().plot(
 plt.show()
 
 # ----------------------------
-# 📊 4. Distribuição de valores
+# 4. Distribuição de valores
 # ----------------------------
 df["amount"].hist(bins=50)
 plt.title("Distribuição de valores")
 plt.show()
 
 # ----------------------------
-# 📊 5. Fraude por categoria
+# 5. Fraude por categoria
 # ----------------------------
 df.groupby("merchant_category")["is_fraud"].mean().plot(
     kind="bar",
