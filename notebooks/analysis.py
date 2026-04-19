5e48895b9924517ec16df9975b77b84058fa5aa6
Magna Tercillia <estudotopne@outlook.com>
2026-04-19 15:37:25 -0300

    graficos

--- notebooks/analysis.py
+++ notebooks/analysis.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+# estilo profissional
+sns.set(style="darkgrid")
 
 # carregar dataset
 df = pd.read_csv("data/transactions.csv")
@@ -7,48 +11,44 @@ df = pd.read_csv("data/transactions.csv")
 print("\nPrimeiras linhas:")
 print(df.head())
 
-print("\nPrimeiras linhas:")
-print(df.info())
-
 print("\nDistribuição de fraude:")
 print(df["is_fraud"].value_counts())
 
 # ----------------------------
-# 1. Valor médio por fraude
+# 📊 1. Distribuição de fraude
 # ----------------------------
-print("\nMédia de valores:")
-print(df.groupby("is_fraud")["amount"].mean())
-
-# ----------------------------
-# 2. Quantidade de fraudes por usuário
-# ----------------------------
-fraud_by_user = df.groupby("user_id")["is_fraud"].sum().sort_values(ascending=False)
-
-print("\nTop usuários com fraude:")
-print(fraud_by_user.head())
-
-# ----------------------------
-# 3. Gráfico de fraude vs não fraude
-# ----------------------------
-df["is_fraud"].value_counts().plot(
-    kind="bar",
-    title="Fraude vs Não Fraude",
-    color=["green", "red"]
-)
+plt.figure(figsize=(6,4))
+sns.countplot(data=df, x="is_fraud", palette="Set2")
+plt.title("Fraude vs Não Fraude")
 plt.show()
 
 # ----------------------------
-# 4. Distribuição de valores
+# 📊 2. Distribuição de valores
 # ----------------------------
-df["amount"].hist(bins=50)
-plt.title("Distribuição de valores")
+plt.figure(figsize=(8,5))
+sns.histplot(df["amount"], bins=50, kde=True, color="blue")
+plt.title("Distribuição dos Valores das Transações")
 plt.show()
 
 # ----------------------------
-# 5. Fraude por categoria
+# 📊 3. Fraude por categoria
 # ----------------------------
-df.groupby("merchant_category")["is_fraud"].mean().plot(
-    kind="bar",
-    title="Taxa de fraude por categoria"
+plt.figure(figsize=(10,5))
+sns.barplot(
+    data=df,
+    x="merchant_category",
+    y="is_fraud",
+    estimator="mean",
+    palette="Reds"
 )
+plt.title("Taxa de Fraude por Categoria")
+plt.xticks(rotation=45)
+plt.show()
+
+# ----------------------------
+# 📊 4. Relação valor vs fraude
+# ----------------------------
+plt.figure(figsize=(7,5))
+sns.boxplot(data=df, x="is_fraud", y="amount", palette="Set3")
+plt.title("Valor vs Fraude")
 plt.show()
